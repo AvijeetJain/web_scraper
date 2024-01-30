7e16a7e3be9c102f7e9721fa0fd7194846b854f6
AvijeetJain <jainavijeet@gmail.com>
2024-01-30 19:33:09 +0530

    Unnecessary Import Removed

--- newcode.py
+++ newcode.py
@@ -5,7 +5,6 @@ import time
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 
